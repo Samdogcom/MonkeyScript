a4062f9f7eef11dbc666d9cc70b41c66c08ddf2d
Samdogcom <samvongz@hotmail.com>
2023-09-10 05:45:52 +0800

    常用影视站添加跳转豆瓣搜索功能

--- DoubanPG/DoubanPG.user.js
+++ DoubanPG/DoubanPG.user.js
@@ -2,11 +2,19 @@
 // @name         豆瓣电影分级、搜索、跳转
 // @namespace    Sam/DoubanPG
 // @homepageURL  https://github.com/Samdogcom/MonkeyScript
-// @version      0.2
+// @version      0.3
 // @description  修改自 https://greasyfork.org/zh-CN/scripts/429162-豆瓣电影分级 ，增加会剧透的分级描述，兼容豆瓣助手，增加电影站与豆瓣的相互跳转
 // @author       Sam
 // @icon         https://img3.doubanio.com/favicon.ico
 // @match        *://movie.douban.com/subject/*
+// @match        *://*.hao6v.tv/*
+// @match        *://*.6v520.com/*
+// @match        *://*.6vdy.cc/*
+// @match        *://*.6vw.cc/*
+// @match        *://*.ygdy8.net/*
+// @match        *://*.dytt8.net/*
+// @match        *://*.xiaopian.com/*
+// @match        *://*.dy2018.com/*
 // @grant        GM_xmlhttpRequest
 // @connect      www.imdb.com
 // @require      https://fastly.jsdelivr.net/npm/encode-gb2312@0.0.2/encodeToGb2312.min.js
@@ -20,18 +28,18 @@
     //https://www.imdb.com/title/tt15398776/parentalguide
 
     const logTag = "Sam: ";
-	const url = location.href;
-	const head = document.head;
+    const url = location.href;
+    const head = document.head;
 
-	var info_item;
-	var imdb_item;
-	var imdb_id_item;
-	var imdb_id;
-	var douban_id;
-	var douban_cn_name;
+    var info_item;
+    var imdb_item;
+    var imdb_id_item;
+    var imdb_id;
+    var douban_id;
+    var douban_cn_name;
     var douban_cn_name_gbk;
-	var douban_all_name;
-	var douban_en_name;
+    var douban_all_name;
+    var douban_en_name;
 
     function get_imdb_item() {
         //获取imdbitem
@@ -146,22 +154,22 @@
         let s = "";
         for (var i = 0; i < tmp.length; i++) {
             let title = tmp[i].getElementsByClassName("ipl-list-title")[0].textContent;
-            if (title.toLowerCase().indexOf("nudity") != -1) {
+            if (title.toLowerCase().includes("nudity")) {
                 title = "性爱和裸体:"
-            } else if (title.toLowerCase().indexOf("violence") != -1) {
+            } else if (title.toLowerCase().includes("violence")) {
                 title = "暴力和血腥:"
-            } else if (title.toLowerCase().indexOf("profanity") != -1) {
+            } else if (title.toLowerCase().includes("profanity")) {
                 title = "粗言俗语:"
-            } else if (title.toLowerCase().indexOf("alcohol") != -1) {
+            } else if (title.toLowerCase().includes("alcohol")) {
                 title = "酒精毒品和烟草:"
-            } else if (title.toLowerCase().indexOf("frightening") != -1) {
+            } else if (title.toLowerCase().includes("frightening")) {
                 title = "恐怖和紧张场景:"
             }
             s += title + "<br>";
 
             let content = tmp[i].getElementsByClassName("ipl-zebra-list__item");
             for (var j = 0; j < content.length; j++) {
-                    s += (j + 1) + "." + content[j].textContent + "<br>";
+                s += (j + 1) + "." + content[j].textContent + "<br>";
             }
         }
         return s;
@@ -187,67 +195,67 @@
     }
 
     /**
-	 * 恢复IMDB链接
-	 */
-	function imdb_link() {
-		let div = document.createElement("div");
-		div.innerHTML = "<span class='pl'>IMDb:</span><a target='_blank' href='https://www.imdb.com/title/" + imdb_id + "'>&nbsp" + imdb_id + "</a><br>";
-		imdb_id_item.after(div);
-		//删除原本的imdb链接
-		imdb_item.remove();
-		imdb_id_item.remove();
+     * 恢复IMDB链接
+     */
+    function imdb_link() {
+        let div = document.createElement("div");
+        div.innerHTML = "<span class='pl'>IMDb:</span><a target='_blank' href='https://www.imdb.com/title/" + imdb_id + "'>&nbsp" + imdb_id + "</a><br>";
+        imdb_id_item.after(div);
+        //删除原本的imdb链接
+        imdb_item.remove();
+        imdb_id_item.remove();
 
         //原来的删除了,所以要重新赋值,imdb_id_item已经无用
         imdb_item = div.firstChild;
-	}
+    }
 
     /**
-	 * 侧边栏功能列表
-	 */
-	const webSites = [
-		{
-			id: "group1",
-			name: "影视资源",
-			links: [
-				{
-					name: "555电影",
-					url: "555dyx1.com",
-					search: "https://555dyx1.com/vodsearch/-------------.html?wd=",
-					id: "cn",
-				},
-				{
+     * 侧边栏功能列表
+     */
+    const webSites = [
+        {
+            id: "group1",
+            name: "影视资源",
+            links: [
+                {
+                    name: "555电影",
+                    url: "555dyx1.com",
+                    search: "https://555dyx1.com/vodsearch/-------------.html?wd=",
+                    id: "cn",
+                },
+                {
                     name: "哔嘀影视",
-					url: "bdys.me",
-					search: "https://www.bdys10.com/search/",
-					id: "cn",
-				},
-				{
-					name: "茶杯狐",
-					url: "cupfox.app",
-					search: "https://cupfox.app/s/",
-					id: "cn",
-				},
-				{
-					name: "WebHD",
-					url: "webhd.top",
-					search: "https://webhd.top/search/",
-					id: "cn",
-				},
-				{
-					name: "MINI4K",
-					url: "mini4k.org",
-					search: "https://www.mini4k.org/search?term=",
-					id: "cn",
-				},
-				{
-					name: "BTNULL",
-					url: "btnull.in",
-					search: "https://www.btnull.in/s/1---1/",
-					id: "cn",
-				},
-			],
-		},
-	];
+                    url: "bdys.me",
+                    search: "https://www.bdys10.com/search/",
+                    id: "cn",
+                },
+                {
+                    name: "茶杯狐",
+                    url: "cupfox.app",
+                    search: "https://cupfox.app/s/",
+                    id: "cn",
+                },
+                {
+                    name: "WebHD",
+                    url: "webhd.top",
+                    search: "https://webhd.top/search/",
+                    id: "cn",
+                },
+                {
+                    name: "MINI4K",
+                    url: "mini4k.org",
+                    search: "https://www.mini4k.org/search?term=",
+                    id: "cn",
+                },
+                {
+                    name: "BTNULL",
+                    url: "btnull.in",
+                    search: "https://www.btnull.in/s/1---1/",
+                    id: "cn",
+                },
+            ],
+        },
+    ];
 
     function getKeyword(link) {
         let keyword = "";
@@ -269,42 +277,76 @@
     }
 
     /**
-	 * 侧边栏功能
-	 */
-	function aside() {
-		let aside = document.querySelector(".aside");
-        console.log(logTag + douban_cn_name);
-        console.log(logTag + douban_cn_name_gbk);
-        console.log(logTag + douban_all_name);
-        console.log(logTag + douban_en_name);
-		for (let webSite of webSites) {
-			let div = document.createElement("div");
-			div.className = "resource";
-			if (true) {
-				div.innerHTML = "<h2><i>" + webSite.name + "</i>· · · · · ·</h2>";
-				aside.prepend(div);
-				let ul = document.createElement("ul");
-				ul.className = "resources";
-				div.appendChild(ul);
-				for (let link of webSite.links) {
-					if (link.type == "xhr") {
-						//特殊处理
-					} else {
-						let str = '<a href="' + link.search + getKeyword(link) + '" target="_blank">' + link.name + "</a>";
-						let a = document.createRange().createContextualFragment(str);
-						ul.appendChild(a);
-					}
-				}
-			}
-		}
+     * 侧边栏功能
+     */
+    function aside() {
+        let aside = document.querySelector(".aside");
+        for (let webSite of webSites) {
+            let div = document.createElement("div");
+            div.className = "resource";
+            if (true) {
+                div.innerHTML = "<h2><i>" + webSite.name + "</i>· · · · · ·</h2>";
+                aside.prepend(div);
+                let ul = document.createElement("ul");
+                ul.className = "resources";
+                div.appendChild(ul);
+                for (let link of webSite.links) {
+                    if (link.type == "xhr") {
+                        //特殊处理
+                    } else {
+                        let str = '<a href="' + link.search + getKeyword(link) + '" target="_blank">' + link.name + "</a>";
+                        let a = document.createRange().createContextualFragment(str);
+                        ul.appendChild(a);
+                    }
+                }
+            }
+        }
 
-		const resourceStyle = document.createElement("style");
-		resourceStyle.innerHTML =
-			".resource {margin-bottom: 30px}  .resources {padding: 0 12px;*letter-spacing: normal}  .resources a {border-radius: 6px;color: #37A;display: inline-block;letter-spacing: normal;margin: 0 8px 8px 0;padding: 0 8px;text-align: center;width: 65px}  .resources a:link, .resources a:visited {background-color: #f5f5f5;color: #37A}  .resources a:hover, .resources a:active {background-color: #e8e8e8;color: #37A}  .resources a.disabled {text-decoration: line-through}  .resources a.available {background-color: #5ccccc;color: #006363}  .resources a.available:hover, .resources a.available:active {background-color: #3cc}  .resources a.honse {background-color: #fff0f5;color: #006363}  .resources a.honse:hover, .resources a.honse:active {background-color: #3cc}  .resources a.sites_r0 {text-decoration: line-through}";
-		document.head.appendChild(resourceStyle);
-	}
+        const resourceStyle = document.createElement("style");
+        resourceStyle.innerHTML =
+            ".resource {margin-bottom: 30px}  .resources {padding: 0 12px;*letter-spacing: normal}  .resources a {border-radius: 6px;color: #37A;display: inline-block;letter-spacing: normal;margin: 0 8px 8px 0;padding: 0 8px;text-align: center;width: 65px}  .resources a:link, .resources a:visited {background-color: #f5f5f5;color: #37A}  .resources a:hover, .resources a:active {background-color: #e8e8e8;color: #37A}  .resources a.disabled {text-decoration: line-through}  .resources a.available {background-color: #5ccccc;color: #006363}  .resources a.available:hover, .resources a.available:active {background-color: #3cc}  .resources a.honse {background-color: #fff0f5;color: #006363}  .resources a.honse:hover, .resources a.honse:active {background-color: #3cc}  .resources a.sites_r0 {text-decoration: line-through}";
+        document.head.appendChild(resourceStyle);
+    }
 
-    if (window.location.href.indexOf("movie.douban.com/subject")!=-1) {
+    function htmlToElement(str) {
+        return document.createRange().createContextualFragment(str);
+    }
+
+    function addDoubanSearch() {
+        let h = document.getElementsByTagName("h1");
+        if(!h || !h[0]){
+            return;
+        }
+        let name = h[0].innerText;
+        if (name.includes("《")) {
+            name = name.split("《")[1];
+            name = name.split("》")[0];
+        } else if (name.includes("【")) {
+            name = name.split("【")[1];
+            name = name.split("】")[0];
+        }
+        let search = "<a target='_blank' href='https://search.douban.com/movie/subject_search?search_text=" + encodeURI(name) + "'; class='lnk-sharing'; style='margin-left: 5px'>豆瓣搜索</a>"
+        h[0].innerHTML += search;
+
+        let img = document.getElementsByTagName("img");
+        search = "<br>" + search
+        for (var i = 0; i < img.length; i++) {
+            let src = img[i].getAttribute('src');
+            let alt = img[i].getAttribute('alt');
+            console.log(logTag + src);
+            if (!alt && alt.trim().length == 0) {
+                let a = htmlToElement(search);
+                if (i == 0) {
+                    img[i].after(a);
+                } else if (i == 1) {
+                    //某些网页beforeBegin区分大小写
+                    img[i].insertAdjacentHTML('beforeBegin', search);
+                }
+            }
+        }
+    }
+
+    if (window.location.href.includes("movie.douban.com/subject")) {
         get_imdb_item();
 
         //兼容豆瓣助手,不是超链接的话就恢复imdb链接
@@ -320,7 +362,9 @@
         }
 
         let parentalguide = "<a target='_blank' href='https://www.imdb.com/title/" + imdb_id + "/parentalguide'; class='lnk-sharing'; style='margin-left: 5px'>直达</a>"
-        imdb_item.nextSibling.nextSibling.insertAdjacentHTML('beforebegin', '<a id="gpg" href="javascript:getPG();" class="lnk-sharing" style="margin-left: 5px;">查看分级</a>' + parentalguide);
+        imdb_item.nextSibling.nextSibling.insertAdjacentHTML('beforeBegin', '<a id="gpg" href="javascript:getPG();" class="lnk-sharing" style="margin-left: 5px;">查看分级</a>' + parentalguide);
         aside();
+    } else {
+        addDoubanSearch()
     }
 })();
